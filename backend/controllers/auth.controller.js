fc8ae397d213f6b1f16464b25c61e4292c0c7813
Arsen Kohut <kohut.arsen@gmail.com>
2024-08-14 13:57:13 +0300

    Create Sign Up endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,53 @@
-const signup = (req, res) => {
-    console.log("SignUp user");
-    res.send("SignUp Route")
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+
+const signup = async (req, res) => {
+    try {
+        const { fullName, username, password, confirmPassword, gender } = req.body;
+
+        if (password !== confirmPassword) {
+            res.status(400).json({ error: "Password don't match"} );
+        }
+
+        const user = await User.findOne({username});
+
+        if (user) {
+            res.send(400).status({ error: "Username already exists"} );
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Profile picture
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        if (newUser) {
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal server error"} );
+    }
 }
 
 const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth.routes');
 const connectToMongoDB = require('./db/connectToMongoDb');
-const app = express();
 
-dotenv.config();
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     res.send("hello world");
+// });
 
 app.listen(PORT, () => {
     connectToMongoDB();
