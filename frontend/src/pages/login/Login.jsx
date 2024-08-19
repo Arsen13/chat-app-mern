acbe72210b9e178d094fecdef9a53226ee957855
Arsen Kohut <kohut.arsen@gmail.com>
2024-08-19 16:19:04 +0300

    Create vite project for frontend. Add all needed library, and create 'Login' page

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,46 @@
+const Login = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+            <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+                <h1 className="text-3xl font-semibold text-center text-gray-300">
+                    Login
+                    <span className="text-blue-500"> ChatApp</span>
+                </h1>
+
+                <form>
+                    <div>
+                        <label className="label p-2" for="username">
+                            <span className="text-base label-text text-blue-200">Username</span>
+                        </label>
+                        <input 
+                            id="username"
+                            type="text" 
+                            placeholder="Enter username" 
+                            className="w-full input input-bordered h-10" 
+                        />
+                    </div>
+
+                    <div>
+                        <label className="label p-2" for="password">
+                            <span className="text-base label-text text-blue-200">Password</span>
+                        </label>
+                        <input 
+                            id="password"
+                            type="password"
+                            placeholder="Enter password"
+                            className="w-full input input-bordered h-10"
+                        />
+                    </div>
+
+                    <a href="#" className="text-sm hover:text-blue-300 mt-2 inline-block text-blue-200">
+                        {"Don't"} have an account?    
+                    </a> 
+
+                    <button className="btn btn-block btn-sm mt-2">Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
