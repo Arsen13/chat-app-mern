f5e8367840d4a64555565242745b53a93ab81957
Arsen Kohut <kohut.arsen@gmail.com>
2024-08-20 15:51:17 +0300

    Finished 'Home' page. Create components for display messages

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+import { TiMessages } from 'react-icons/ti'
+
+const MessageContainer = () => {
+    const [chatSelected, setChatSelected] = useState(false);
+
+    return (
+        <div className="md:min-w-[450px] flex flex-col">
+            {chatSelected ? (
+                <>
+                    <div className="bg-slate-500 px-4 py-2 mb-2">
+                        <span className="label-text text-gray-200">To:</span>
+                        <span className="text-white font-bold"> John Doe</span>
+                    </div>
+            
+                    <Messages />
+                    <MessageInput />
+                </>
+            ) : (
+                <NoChatSelected />
+            )}
+        </div>
+    )
+}
+
+export default MessageContainer;
+
+const NoChatSelected = () => {
+    return (
+        <div className="flex items-center justify-center w-full h-full">
+            <div className="px-4 text-center sm:text-lg md:texl-lg text-gray-200 font-semibold flex flex-col items-center gap-2">
+                <p>Welcome &#128075; John Doe</p>
+                <p>Select a chat to start messaging</p>
+                <TiMessages className="text-3xl md:text-6xl text-center" />
+            </div>
+        </div>
+    )
+}
