a49481999d48d17a6c7b152f1c7a2d1fbf8c6488
Arsen Kohut <kohut.arsen@gmail.com>
2024-08-26 17:34:02 +0300

    Setting app for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const authRoutes = require('./routes/auth.routes');
 const messageRoutes = require('./routes/message.routes');
@@ -19,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
